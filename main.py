eb4159ea964895f9b6775222bfff56cae46bc1a0
mattallio <matteleo02@gmail.com>
2023-01-26 10:27:04 +0100

    adding jurgen rant command and fixing little bug in specific sticker

--- main.py
+++ main.py
@@ -68,6 +68,40 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
+    async def jurgenRant(message):
+        await message.reply("JURGEN LEITNER?")
+        time.sleep(1)
+        await message.channel.send("STUPID IDIOT MOTHERFUCKING JURGEIN LEITNER GOD DAMN FOOL BOOK COLLECTING DUST EATING RAT OLD BASTARD SHITHEAD IDIOT AVATAR OF THE WHORE BIGGEST CLOWN IN THE CIRCUS LAUGHED OUT OF TOWN COWBOY MOTHERFUCKING JURGEIN LEITNER")
+        time.sleep(1)
+        await message.channel.send("STOP PINNING ME WHEN I TALK ABOUT JURGEIN LEITENER I HATE HIM SO MUCH WHY DOES HE HAVE SO MANY FUCKED UP BOOKS WHY DID HE DECIDE TO FUCK AROUND AND FIND OUT JUST SET THEM LOOSE IS HE DEAD IS HE A BASTARD MAN HAS SUCH A VISCERAL AFFECT ON ME NOT EVEN IN THE ROOM NEVER SEEN THIS MANS FACE AND I KNOW HE HAS THE WORLDS SHITTIEST BEARD GET AWAY FROM ME")
+        time.sleep(1)
+        await message.channel.send("if i wanted to get into heaven and god said jurgein leitner's waiting inside i would piss on gods feet for the sole purpose of getting sent back down")
+        time.sleep(1)
+        await message.channel.send("if i have to deal with jurgein leitner speaking one word in person on voice in podcast not only will i close the tab i will delete my bookmark out of spite and have to rewatch the entire series again for the experience of being able to skip all the times when he is mentioned or alive")
+        time.sleep(1)
+        await message.channel.send("i dont even know why i hate him so much. he collects books but i am just mad because i am angy")
+        time.sleep(1)
+        await message.channel.send("he better have some fucked up backstory to explain this if hes just some rich shithead whos a fan of creepypasta and wanted the irl version ill go ham")
+        time.sleep(1)
+        await message.channel.send("BETTER have had a book make him kill a man cuz if he didnt Im going to make him")
+        time.sleep(1)
+        await message.channel.send("paypal.com/IFuckingHateJurgeinLeitner")
+        time.sleep(1)
+        await message.channel.send("episodes not even about him. vaguely mentioned what is supposed to maybe be his library and I lost it")
+        time.sleep(1)
+        await message.channel.send("where the fuck is jurgein leitner if hes still alive im going to so deeply wish he wasnt")
+        time.sleep(1)
+        await message.channel.send("crusty old man")
+        time.sleep(1)
+        await message.channel.send("ill punch leitner and his sad frail old man twig bones will simply flake apart under my epic huge meat fist and he will disintegrate until all thats left is one final book he kept on him at all times simply titled Now You Fucked Up in ancient yiddish")
+        time.sleep(1)
+        await message.channel.send("im not breathing im hyperventilating at this point")
+        time.sleep(1)
+        await message.channel.send("i hope theres a date given for when jurgen died or will die so i can make it a reminder on my phone")
+        time.sleep(1)
+        await message.channel.send("everyday once a year i will see it and do anything but pay respects to the man who had so many fucked up if true books")
+
     if message.author == client.user:
         return
     
@@ -133,6 +167,9 @@ async def on_message(message):
                 time.sleep(2)
                 await message.channel.send(smites[0][i])
 
+    if "/avatar of the whore" in message.content:
+        jurgenRant(message)
+
     if message.content == "/help":
         helpMessage = open(r"Utilities/help.txt", "r")
         await message.channel.send(helpMessage.read())
@@ -154,12 +191,12 @@ async def on_message(message):
     #if the message.channel is in the timer list the archivist will not send a sticker every time a word that's in a sticker is said
     #the timer list will be resetted every two minutes so that the archivist can send a sticker based on the context every 2 minutes
     if message.channel not in timer:
-        timer.append(message.channel)
         stickerNum = checkSticker(message.content)
         if stickerNum <= countFolder(r"Stickers")-1:
             sticker = open(fr"Stickers/{stickerNum}.webp", "rb")
             await message.channel.send(file = discord.File(sticker))
             sticker.close()
+            timer.append(message.channel)
 
     swearsFile = open("swears.txt", "r")
     swears = (swearsFile.read()).split("\n")
@@ -170,37 +207,7 @@ async def on_message(message):
                 jurgens[message.author.id][0] += 1
                 if jurgens[message.author.id][0] == SWEARSMAX:
                     await message.author.edit(nick = "Jurgen Leitner")
-                    await message.reply("JURGEN LEITNER?")
-                    time.sleep(1)
-                    await message.channel.send("STUPID IDIOT MOTHERFUCKING JURGEIN LEITNER GOD DAMN FOOL BOOK COLLECTING DUST EATING RAT OLD BASTARD SHITHEAD IDIOT AVATAR OF THE WHORE BIGGEST CLOWN IN THE CIRCUS LAUGHED OUT OF TOWN COWBOY MOTHERFUCKING JURGEIN LEITNER")
-                    time.sleep(1)
-                    await message.channel.send("STOP PINNING ME WHEN I TALK ABOUT JURGEIN LEITENER I HATE HIM SO MUCH WHY DOES HE HAVE SO MANY FUCKED UP BOOKS WHY DID HE DECIDE TO FUCK AROUND AND FIND OUT JUST SET THEM LOOSE IS HE DEAD IS HE A BASTARD MAN HAS SUCH A VISCERAL AFFECT ON ME NOT EVEN IN THE ROOM NEVER SEEN THIS MANS FACE AND I KNOW HE HAS THE WORLDS SHITTIEST BEARD GET AWAY FROM ME")
-                    time.sleep(1)
-                    await message.channel.send("if i wanted to get into heaven and god said jurgein leitner's waiting inside i would piss on gods feet for the sole purpose of getting sent back down")
-                    time.sleep(1)
-                    await message.channel.send("if i have to deal with jurgein leitner speaking one word in person on voice in podcast not only will i close the tab i will delete my bookmark out of spite and have to rewatch the entire series again for the experience of being able to skip all the times when he is mentioned or alive")
-                    time.sleep(1)
-                    await message.channel.send("i dont even know why i hate him so much. he collects books but i am just mad because i am angy")
-                    time.sleep(1)
-                    await message.channel.send("he better have some fucked up backstory to explain this if hes just some rich shithead whos a fan of creepypasta and wanted the irl version ill go ham")
-                    time.sleep(1)
-                    await message.channel.send("BETTER have had a book make him kill a man cuz if he didnt Im going to make him")
-                    time.sleep(1)
-                    await message.channel.send("paypal.com/IFuckingHateJurgeinLeitner")
-                    time.sleep(1)
-                    await message.channel.send("episodes not even about him. vaguely mentioned what is supposed to maybe be his library and I lost it")
-                    time.sleep(1)
-                    await message.channel.send("where the fuck is jurgein leitner if hes still alive im going to so deeply wish he wasnt")
-                    time.sleep(1)
-                    await message.channel.send("crusty old man")
-                    time.sleep(1)
-                    await message.channel.send("ill punch leitner and his sad frail old man twig bones will simply flake apart under my epic huge meat fist and he will disintegrate until all thats left is one final book he kept on him at all times simply titled Now You Fucked Up in ancient yiddish")
-                    time.sleep(1)
-                    await message.channel.send("im not breathing im hyperventilating at this point")
-                    time.sleep(1)
-                    await message.channel.send("i hope theres a date given for when jurgen died or will die so i can make it a reminder on my phone")
-                    time.sleep(1)
-                    await message.channel.send("everyday once a year i will see it and do anything but pay respects to the man who had so many fucked up if true books")
+                    jurgenRant(message)
                     jurgens[message.author.id][0] = 0
                 else:
                     randomNum = random.randint(0, len(smites)-1)
